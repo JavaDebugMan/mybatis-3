ad0868b7f47ab606cc9111e726269ea3f59673cb
pengzhe <pengzhepost@sina.com>
2018-04-26 23:34:17 +0800

    Mybatis源码分析

--- src/main/java/org/apache/ibatis/builder/xml/XMLMapperEntityResolver.java
+++ src/main/java/org/apache/ibatis/builder/xml/XMLMapperEntityResolver.java
@@ -1,17 +1,17 @@
 /**
- *    Copyright 2009-2016 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2009-2016 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.ibatis.builder.xml;
 
@@ -26,59 +26,68 @@ import org.xml.sax.SAXException;
 
 /**
  * Offline entity resolver for the MyBatis DTDs
- * 
+ * 默认情况下,对XML文档进行验证时,会根据XML文档开始位置指定的网址加载对应的DTD或者XSD文件
+ * 如果解析mybatis-config.xml配置文件,默认联网加载http://mybatis.org/dt/mybatis-3-config.dtd这个DTD文件
+ * 当网络较慢时会导致验证过程缓慢,在实践过程中往往会提前设置EntityResolver接口对象加载本地的DTD文件,从而避免互联网加载DTD文件
+ * XMLMapperEntityResolver是Mybatis提供的EntityResolver接口的实现类
+ *
  * @author Clinton Begin
  * @author Eduardo Macarron
  */
 public class XMLMapperEntityResolver implements EntityResolver {
+    /**
+     * 指定mybatis-config.xml和映射文件对应的DTD文件的SystemId
+     */
+    private static final String IBATIS_CONFIG_SYSTEM = "ibatis-3-config.dtd";
+    private static final String IBATIS_MAPPER_SYSTEM = "ibatis-3-mapper.dtd";
+    private static final String MYBATIS_CONFIG_SYSTEM = "mybatis-3-config.dtd";
+    private static final String MYBATIS_MAPPER_SYSTEM = "mybatis-3-mapper.dtd";
+    /**
+     * 指定mybatis-config.xml和映射文件对应的DTD文件的具体位置
+     */
+    private static final String MYBATIS_CONFIG_DTD = "org/apache/ibatis/builder/xml/mybatis-3-config.dtd";
+    private static final String MYBATIS_MAPPER_DTD = "org/apache/ibatis/builder/xml/mybatis-3-mapper.dtd";
 
-  private static final String IBATIS_CONFIG_SYSTEM = "ibatis-3-config.dtd";
-  private static final String IBATIS_MAPPER_SYSTEM = "ibatis-3-mapper.dtd";
-  private static final String MYBATIS_CONFIG_SYSTEM = "mybatis-3-config.dtd";
-  private static final String MYBATIS_MAPPER_SYSTEM = "mybatis-3-mapper.dtd";
-
-  private static final String MYBATIS_CONFIG_DTD = "org/apache/ibatis/builder/xml/mybatis-3-config.dtd";
-  private static final String MYBATIS_MAPPER_DTD = "org/apache/ibatis/builder/xml/mybatis-3-mapper.dtd";
-
-  /*
-   * Converts a public DTD into a local one
-   * 
-   * @param publicId The public id that is what comes after "PUBLIC"
-   * @param systemId The system id that is what comes after the public id.
-   * @return The InputSource for the DTD
-   * 
-   * @throws org.xml.sax.SAXException If anything goes wrong
-   */
-  @Override
-  public InputSource resolveEntity(String publicId, String systemId) throws SAXException {
-    try {
-      if (systemId != null) {
-        String lowerCaseSystemId = systemId.toLowerCase(Locale.ENGLISH);
-        if (lowerCaseSystemId.contains(MYBATIS_CONFIG_SYSTEM) || lowerCaseSystemId.contains(IBATIS_CONFIG_SYSTEM)) {
-          return getInputSource(MYBATIS_CONFIG_DTD, publicId, systemId);
-        } else if (lowerCaseSystemId.contains(MYBATIS_MAPPER_SYSTEM) || lowerCaseSystemId.contains(IBATIS_MAPPER_SYSTEM)) {
-          return getInputSource(MYBATIS_MAPPER_DTD, publicId, systemId);
+    /*
+     * Converts a public DTD into a local one
+     *
+     * @param publicId The public id that is what comes after "PUBLIC"
+     * @param systemId The system id that is what comes after the public id.
+     * @return The InputSource for the DTD
+     *
+     * @throws org.xml.sax.SAXException If anything goes wrong
+     */
+    @Override
+    public InputSource resolveEntity(String publicId, String systemId) throws SAXException {
+        try {
+            if (systemId != null) {
+                String lowerCaseSystemId = systemId.toLowerCase(Locale.ENGLISH);
+                //查找SystemId指定的DTD文档,并调用getInputSource()方法读取DTD文档
+                if (lowerCaseSystemId.contains(MYBATIS_CONFIG_SYSTEM) || lowerCaseSystemId.contains(IBATIS_CONFIG_SYSTEM)) {
+                    return getInputSource(MYBATIS_CONFIG_DTD, publicId, systemId);
+                } else if (lowerCaseSystemId.contains(MYBATIS_MAPPER_SYSTEM) || lowerCaseSystemId.contains(IBATIS_MAPPER_SYSTEM)) {
+                    return getInputSource(MYBATIS_MAPPER_DTD, publicId, systemId);
+                }
+            }
+            return null;
+        } catch (Exception e) {
+            throw new SAXException(e.toString());
         }
-      }
-      return null;
-    } catch (Exception e) {
-      throw new SAXException(e.toString());
     }
-  }
 
-  private InputSource getInputSource(String path, String publicId, String systemId) {
-    InputSource source = null;
-    if (path != null) {
-      try {
-        InputStream in = Resources.getResourceAsStream(path);
-        source = new InputSource(in);
-        source.setPublicId(publicId);
-        source.setSystemId(systemId);        
-      } catch (IOException e) {
-        // ignore, null is ok
-      }
+    private InputSource getInputSource(String path, String publicId, String systemId) {
+        InputSource source = null;
+        if (path != null) {
+            try {
+                InputStream in = Resources.getResourceAsStream(path);
+                source = new InputSource(in);
+                source.setPublicId(publicId);
+                source.setSystemId(systemId);
+            } catch (IOException e) {
+                // ignore, null is ok
+            }
+        }
+        return source;
     }
-    return source;
-  }
 
 }
